b6ab150b76767ceefa597d9b4b883c3d9b2bb4d0
rpawar19 <rajendra2000@ymail.com>
2022-07-05 18:40:59 +0530

    RP change 3

--- src/main/java/com/demogit/webpages/hello.java
+++ src/main/java/com/demogit/webpages/hello.java
@@ -4,10 +4,11 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello");
-System.out.println("demo");
-System.out.println("demo2 - rp personal");
-System.out.println("rp- rp personal1");
+		System.out.println("hello");
+		System.out.println("demo");
+		System.out.println("demo2 - rp personal");
+		System.out.println("rp- rp personal1");
+		System.out.println("Changes after stash");
 	}
 
 }
